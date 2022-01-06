3fb125d8e99c996fb6167cdeb55358cf8565b4a3
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-01-06 09:25:38 -0500

    Add misc command to add the verified role to all current members

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,5 +1,10 @@
 import nextcord
 from nextcord.ext import commands
+from pathlib import Path
+from toml import load
+from typing import Optional
+
+CONFIG = load(Path("configs/config.toml"))
 
 
 class DeclarationURL(nextcord.ui.View):
@@ -15,10 +20,29 @@ class DeclarationURL(nextcord.ui.View):
 class Miscellaneous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.activist_role: Optional[nextcord.Role] = None
 
-    @nextcord.slash_command(description="Declaration on the Rights of the Villager")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.activist_role = self.bot.get_role(CONFIG["IDS"]["ACTIVIST_ROLE"])
+
+    @nextcord.slash_command(
+        name="declaration",
+        description="Declaration on the Rights of the Villager",
+        guild_ids=[CONFIG["IDS"]["GUILD"]]
+    )
     async def declaration(self, interaction: nextcord.Interaction):
         await interaction.response.send_message(
             content="_ _",
             view=DeclarationURL()
         )
+
+    @nextcord.slash_command(
+        name="add_activist_role",
+        description="Add the activist role for all allowed users",
+        guild_ids=[CONFIG["IDS"]["GUILD"]]
+    )
+    async def add_activist_role(self, interaction: nextcord.Interaction):
+        for member in interaction.guild.members:
+            if not member.bot:
+                await member.add_roles(self.activist_role)
